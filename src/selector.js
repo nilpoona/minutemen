1a0c94d94f20a208ad79c21035972eaa50ae259c
YutakaHorikawa <gozutakemaru@gmail.com>
2016-12-19 01:15:56 -0800

    update: remove console.log

--- src/selector.js
+++ src/selector.js
@@ -8,9 +8,7 @@ class Selector {
         const keys = Object.keys(this.routing);
         for (let i = 0, len = keys.length; i < len; i++) {
             let key = keys[i];
-            console.log(key);
             const routes = this.routing[key];
-            console.log(routes);
             if (routes.component === name ) {
                 return { uri: key, component: routes.index};
             }
